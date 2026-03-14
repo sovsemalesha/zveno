3a2ec0025b1090391d3a5f909de079cdffe279fc
sovsemalesha <xam1654@mail.ru>
2026-03-14 12:24:55 +0300

    Add standalone output for Next.js

--- zveno/client/next.config.js
+++ zveno/client/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api',
     NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001',
